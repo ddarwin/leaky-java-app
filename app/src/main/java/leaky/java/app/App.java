81dc0af13a4739f8207c264cd55b5fe713c2d11f
ddarwin <ddarwin@users.noreply.github.com>
2023-12-11 13:03:47 -0500

    Update App.java

--- app/src/main/java/leaky/java/app/App.java
+++ app/src/main/java/leaky/java/app/App.java
@@ -16,12 +16,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Map<String, String> envMap = System.getenv();
-
-        // for (String envName : envMap.keySet()) {
-        //     System.out.format("%s = %s%n", envName, envMap.get(envName));
-        // }
-
         FailureFlags gremlin = new GremlinFailureFlags();
         Experiment[] activeExperiments;
         
